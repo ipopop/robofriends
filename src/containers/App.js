b7ff3cbc1ab236f08ca04946e9e27e635c586361
iPopop <popop@ntymail.com>
2020-08-10 00:23:44 +0200

    refactor code & add Scroll component - video 04-078 - ok

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+import { robots } from '../robots'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: robots }))
+  }
+
+  onsearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const { robots, searchfield } = this.state
+    const filteredRobots = robots.filter((robots) => {
+      return robots.username
+        .toLowerCase()
+        .includes(searchfield.toLowerCase())
+    })
+    
+    return (!robots.length) ?
+      <h1 className='tc'>Loading...</h1> :
+     (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onsearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      )
+    
+  }
+}
+
+export default App
